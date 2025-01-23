540178de2a6d5bf7c4994070dc85aae7b5d3e2f0
David Cartaya Rodríguez <dcartaya77@gmail.com>
2025-01-23 01:32:40 -0500

    config vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   devToolbar: {
     enabled: false
   },
